7849ed5d69df58c6e1594d9827b50c388188f660
Diego Cavalcanti <cavalcantidiego@hotmail.com>
2025-01-12 23:18:57 -0300

    new updates for test

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const listaDeNotasEAlunos = [
     { nota: 8, nome: 'Edna' },
     { nota: 4, nome: 'Bruna' },
     { nota: 6, nome: 'Diego' }
+    { nota: 8, nome: 'Davi' }
 ];
 
 const filtrarAlunosAprovados = alunos => alunos.filter(aluno => aluno.nota >= 6);
